6b5d1795bf00432773bad66cb393deffc6867c55
Simon Dragsbæk Petersen <dev@nomis.dk>
2015-02-04 09:55:34 +0100

    Improve the file mover
    Adds prefix option, fixes naming falure + optimizes the path selection

--- movefiles.js
+++ movefiles.js
@@ -1,15 +1,23 @@
 // Core node modules
 var fs = require('fs');
 var path = require('path');
+//
+var input = path.join(__dirname + "/img");
+var output = path.join(__dirname + "/small");
 
-// Local variables
-var inputFolder = path.join(__dirname + "/input");
-var outputFolder = path.join(__dirname + "/output");
+// leave first element in array as dot to move all
+// var makers = ["."];
+var filter = ["Alfa-Romeo","Aston-Martin","Audi","BMW","Bentley","Buick","Cadillac","Chevrolet","Chrysler","Citroën","Dacia","Daewoo","Daihatsu","Dodge","Ferrari","Fiat","Ford","Honda","Hummer","Hyundai","Infiniti","Iveco","Jaguar","Jeep","Kia","Lada","Lamborghini","Lancia","Land Rover","Lexus","Lotus","Maserati","Mazda","Mercedes-Benz","Mini","Mitsubishi","Nissan","Opel","Peugeot","Pontiac","Porsche","Renault","Rolls-Royce","Rover","Saab","Seat","Skoda","Smart","SsangYong","Subaru","Suzuki","Tesla","Toyota","Volkswagen","Volvo"];
 
-// if the images contains these words they will get moved
-var filter = ["red", "blue", "green"];
+// leave as empty string if not set
+// var filter2 = "";
+var filter2 = "150";
 
-function moveFilesFilter(inputFolder, outputFolder, filter) {
+// leave as empty string if not set
+// var prefix = "";
+var prefix = "small-";
+
+function moveFilesFilter(inputFolder, outputFolder, filter, filter2, prefix) {
 	var filterArr = [];
 	for (var i = 0; i < filter.length; i++) {
 	    filterArr.push(filter[i].toLowerCase());
@@ -37,13 +45,20 @@ function moveFilesFilter(inputFolder, outputFolder, filter) {
 		for (var j = 0; j < filterArr.length; j++) {
 			var filter = filterArr[j];
 			if (source.indexOf(filter) > -1) {
-				var readFile = fs.createReadStream(source);
-				var fileName = source.slice(42);
-				var writeFile = fs.createWriteStream(outputFolder + fileName);
-				readFile.pipe(writeFile);
+				if(source.indexOf(filter2) > -1) {
+					var readFile = fs.createReadStream(source);
+					var fileName = source.split(/[\s\/]+/);
+					if(prefix.length > 0) {
+						var file = outputFolder + "/" + prefix + fileName[fileName.length-1];
+					} else {
+						var file = outputFolder + "/" + fileName[fileName.length-1];
+					}
+					var writeFile = fs.createWriteStream(file);
+					readFile.pipe(writeFile);
+				}
 			}
 		};
 	};
 };
 
-moveFilesFilter(inputFolder, outputFolder, filter);
+moveFilesFilter(input, output, filter, filter2, prefix);
